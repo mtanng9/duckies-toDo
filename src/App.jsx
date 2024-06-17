4a33e6955459a120c61ebf5ae0483e07f3686ef5
mtanng9 <mlt152498@gmail.com>
2024-06-17 15:41:12 -0400

    added code to display todos on webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState} from 'react'
 import './App.css'
+import Todo from './Todo'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todayTodos, setTodayTodos] = useState([{task:"Loading", dueDate:"Loading"}]);
+  const [upcomingTodos, setUpcomingTodos] = useState([{task:"Loading Upcoming", dueDate:"Loading Upcoming"}]);
+  const [pastDueTodos, setPastDueTodos] = useState([{task:"Loading Pastdue", dueDate:"Loading Pastdue"}]);
+  const [completedTodos, setCompletedTodos] = useState([{task:"Loading Completed", dueDate:"Loading Completed"}]);
+ 
+
+  useEffect(() => {
+    fetchToday()
+    fetchUpcoming()
+    fetchPastDue()
+    fetchCompleted()
+  }, [])
+ 
+
+  const fetchToday = async () => {
+    await fetch("http://localhost:3000/todo/today")
+      .then((response) => response.json()) // convert expected "response" to json format 
+      .then((response) => setTodayTodos(response))
+      .catch((error) => console.error(error));
+  }
+
+  const fetchUpcoming = async () => {
+    await fetch("http://localhost:3000/todo/upcoming")
+      .then((response) => response.json()) // convert expected "response" to json format 
+      .then((response) => setUpcomingTodos(response))
+      .catch((error) => console.error(error));
+  }
+
+  const fetchPastDue = async () => {
+    await fetch("http://localhost:3000/todo/pastDue")
+      .then((response) => response.json()) // convert expected "response" to json format 
+      .then((response) => setPastDueTodos(response))
+      .catch((error) => console.error(error));
+  }
+
+  const fetchCompleted = async() => {
+    await fetch("http://localhost:3000/todo/completed")
+    .then((response) => response.json()) // convert expected "response" to json format 
+    .then((response) => setCompletedTodos(response))
+    .catch((error) => console.error(error));
+  }
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+          <h1>Duckies To-Do</h1>
+
+            <div>
+              <h2>Today</h2>
+              <br/>
+              {todayTodos.map( (todayTodo) => {
+                return <Todo todo={todayTodo}/>
+              })}
+           </div>
+
+
+            <div>
+              <h2>Upcoming</h2>
+              <br/>
+              {upcomingTodos.map( (upcomingTodo) => {
+                return <Todo todo={upcomingTodo}/>
+              })}
+            </div>
+
+            <div>
+              <h2>Past Due</h2>
+              <br/>
+              {pastDueTodos.map( (pastDueTodo) => {
+                return <Todo todo={pastDueTodo}/>
+              })}
+            </div>
+
+            <div>
+              <h2>Completed</h2>
+              <br/>
+              {completedTodos.map( (completedTodo) => {
+                return <Todo todo={completedTodo}/>
+              })}
+            </div>
+
+      </div>  
   )
 }
 
